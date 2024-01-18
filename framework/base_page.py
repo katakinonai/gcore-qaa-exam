82013cee2363c8e3828fbe74e2c3cbf1f0d9f7be
Vitalii Bekshnev <vy.bekshnev@gmail.com>
2024-01-18 20:48:42 +0500

    Add pagepbject constructors

--- framework/base_page.py
+++ framework/base_page.py
@@ -1,3 +1,15 @@
+import logging
+from framework.base_element import BaseElement
+
+
 class BasePage(object):
-    def __init__(self, driver):
-        self.driver = driver
+    _uniqueElement: BaseElement
+    _name: str
+
+    def __init__(self, unique_element, name):
+        self._uniqueElement = unique_element
+        self._name = name
+
+    def is_page_open(self, driver):
+        logging.info(f"Check if the page '{self._name}' is open")
+        return self._uniqueElement.is_displayed(driver)
--- tests/pages/login.py
+++ tests/pages/login.py
@@ -1,3 +1,4 @@
+import email
 import logging
 import os
 
@@ -13,6 +14,7 @@ load_dotenv()
 
 class LoginPage(BasePage):
     def __init__(self):
+        super().__init__("//input[@id='index_email']", "email input")
         self.email_input = Input("//input[@id='index_email']", "email input")
         self.password_input = Input("//input[@type='password']", "password input")
         self.email_submit_button = Button(
