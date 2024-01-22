bef5601e79df685a030b92815b1fba24df96da42
Vitalii Bekshnev <vy.bekshnev@gmail.com>
2024-01-22 13:49:45 +0500

    Fix button constructor

--- framework/elements/button.py
+++ framework/elements/button.py
@@ -2,5 +2,5 @@ from framework.base_element import BaseElement
 
 
 class Button(BaseElement):
-    def __init__(self, driver, locator: str, name: str):
-        super().__init__(driver, locator, name)
+    def __init__(self, _driver, _locator: str, _name: str):
+        super().__init__(_driver, _locator, _name)
