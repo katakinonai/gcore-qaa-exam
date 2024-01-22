90af8c672f77f697b08159320e38e0b93caed19a
Vitalii Bekshnev <vy.bekshnev@gmail.com>
2024-01-22 13:49:12 +0500

    Fix label constructor

--- framework/elements/label.py
+++ framework/elements/label.py
@@ -2,5 +2,5 @@ from framework.base_element import BaseElement
 
 
 class Label(BaseElement):
-    def __init__(self, _locator: str, _name: str):
-        super().__init__(_locator, _name)
+    def __init__(self, _driver, _locator: str, _name: str):
+        super().__init__(_driver, _locator, _name)
